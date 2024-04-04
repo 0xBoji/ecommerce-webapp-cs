8379f737801d3856e83f2906958a6284a9bda1fb
ThaiNguyen200104 <quangthai0997@gmail.com>
2024-04-04 17:20:55 +0700

    Feat & UI

--- ecommerce-webapp-cs/Program.cs
+++ ecommerce-webapp-cs/Program.cs
@@ -14,52 +14,52 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddAuthentication(options =>
 {
-	options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-	options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
 })
-	//cookie
+//cookie
 .AddCookie(options =>
 {
-	options.LoginPath = "/auth/google-login";
+    options.LoginPath = "/auth/google-login";
 })
 //google api
 .AddGoogle(options =>
 {
-	options.ClientId = builder.Configuration["GoogleKeys:ClientId"];
-	options.ClientSecret = builder.Configuration["GoogleKeys:ClientSecret"];
+    options.ClientId = builder.Configuration["GoogleKeys:ClientId"];
+    options.ClientSecret = builder.Configuration["GoogleKeys:ClientSecret"];
 });
 
 builder.Services.AddSwaggerGen(c =>
 {
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-	options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
-	options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
 });
 
 builder.Services.AddCors(options =>
 {
-	options.AddPolicy("AllowAllOrigins", builder =>
-	{
-		builder.AllowAnyOrigin()
-			   .AllowAnyMethod()
-			   .AllowAnyHeader();
-	});
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
 });
 //connectionstring here
 builder.Services.AddDbContext<ArtsContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
 ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
-	options.IdleTimeout = TimeSpan.FromMinutes(30);
-	options.Cookie.HttpOnly = true;
-	options.Cookie.IsEssential = true;
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
 });
 
 
@@ -67,14 +67,14 @@ builder.Services.AddSession(options =>
 var app = builder.Build();
 app.UseEndpoints(endpoints =>
 {
-	endpoints.MapControllers();
-	endpoints.MapHub<ChatHub>("/chatHub");
+    endpoints.MapControllers();
+    endpoints.MapHub<ChatHub>("/chatHub");
 });
 
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseExceptionHandler("/error");
